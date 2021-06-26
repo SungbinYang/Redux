3f9a621e48b6b0e32398234b8e3be8742c3c5def
Robert.Yang <ysb05222@naver.com>
2021-06-27 01:35:17 +0900

    react-router-dom과 redux 함께 쓰기
    react-router-dom과 redux 함께 쓰기

--- redux-start/src/containers/UserListContainer.jsx
+++ redux-start/src/containers/UserListContainer.jsx
@@ -2,7 +2,7 @@ import { useCallback } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import UserList from "../components/UserList";
-import { getUsersPromise } from "../redux/modules/users";
+import { getUsersThunk } from "../redux/modules/users";
 
 export default function UserListContainer() {
   const users = useSelector((state) => state.users.data);
@@ -19,7 +19,7 @@ export default function UserListContainer() {
   // }, [dispatch]);
 
   const getUsers = useCallback(() => {
-    dispatch(getUsersPromise());
+    dispatch(getUsersThunk());
   }, [dispatch]);
 
   return <UserList users={users} getUsers={getUsers} />;
